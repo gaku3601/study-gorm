23e713701933de9049c0b3530f01c626ef840d75
gaku3601 <pro.gaku@gmail.com>
2018-05-01 08:31:49 +0900

    日付も入力できるように実装

--- example2/main.go
+++ example2/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
@@ -19,10 +20,23 @@ func main() {
 		Isman: true,
 		Careers: []Career{
 			{
+				Fromdate:    time.Date(2012, 4, 1, 23, 59, 59, 0, time.Local),
+				Todate:      time.Date(2014, 3, 15, 23, 59, 59, 0, time.Local),
 				Description: "大学生活最高！",
 			},
 			{
+				Fromdate:    time.Date(2014, 4, 1, 23, 59, 59, 0, time.Local),
+				Todate:      time.Date(2016, 8, 20, 23, 59, 59, 0, time.Local),
 				Description: "しゃちく生活万歳！",
+			},
+			{
+				Fromdate:    time.Date(2016, 9, 1, 23, 59, 59, 0, time.Local),
+				Todate:      time.Date(2017, 5, 7, 23, 59, 59, 0, time.Local),
+				Description: "ニート生活最高ぅううううううあsぢうふぁすdfh(｀・ω・´)！",
+			},
+			{
+				Fromdate:    time.Date(2017, 5, 8, 23, 59, 59, 0, time.Local),
+				Description: "不動産屋さん！しゃちく万歳！万歳！",
 			},
 		},
 	}
@@ -44,6 +58,8 @@ type Admin struct {
 // 管理者経歴テーブル
 type Career struct {
 	ID          int
+	Fromdate    time.Time
+	Todate      time.Time
 	Description string
 	AdminId     int // AdminsIdではダメ。 AdminIdでテーブルカラムを作成する必要がある
 }
