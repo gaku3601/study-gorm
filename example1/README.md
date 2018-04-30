64db835e9087dc659dffca6ad9df819cf0f28671
gaku3601 <pro.gaku@gmail.com>
2018-05-01 07:38:49 +0900

    簡単なgormの使い方を実装

--- example1/README.md
+++ example1/README.md
@@ -13,3 +13,11 @@ migrationツールにgooseを使用している。
 
 dbフォルダ内に新たに、migrationファイルが生成されるので編集する。
 編集後、goose upコマンドでdbにテーブルが作成されるので、正常に作成されたか確認しておく。
+
+# 出力結果
+
+    &main.Admin{ID:7, Name:"gakuko", Age:15, Isman:false}
+    &main.Admin{ID:6, Name:"gakuko", Age:15, Isman:false}
+    &main.Admin{ID:5, Name:"gakuko", Age:15, Isman:false}
+    ----------------------
+    &main.Admin{ID:1, Name:"gaku", Age:26, Isman:true}
--- example1/main.go
+++ example1/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	db, _ := gorm.Open("postgres",
+		"user=postgres dbname=app sslmode=disable")
+	defer db.Close()
+
+	// insert
+	admins := []*Admin{
+		{Name: "gaku", Age: 26, Isman: true},
+		{Name: "gakuko", Age: 15, Isman: false},
+		{Name: "gakuko", Age: 15, Isman: false},
+		{Name: "gakuko", Age: 15, Isman: false},
+		{Name: "gakuko", Age: 15, Isman: false},
+		{Name: "gakuko", Age: 15, Isman: false},
+		{Name: "gakuko", Age: 15, Isman: false},
+	}
+	for _, v := range admins {
+		db.Create(&v)
+	}
+
+	// fetch
+	var list []*Admin
+	db.Where("name LIKE ?", "%ko%").Order("id DESC").Offset(0).Limit(3).Find(&list)
+	for _, v := range list {
+		fmt.Printf("%#v\n", v)
+	}
+	fmt.Println("----------------------")
+
+	// update
+	admin := &Admin{}
+	db.First(&admin, 3) // where id = 3と同じ
+
+	admin.Name = "gakukokokokokokokaaaaaaaa"
+	db.Save(&admin) // 更新対象が存在しない場合、SAVEは新規作成を実施する
+
+	// where delete
+	db.Where("name Like ?", "%ko%").Delete(&Admin{})
+	db.Delete(&Admin{}, 5) // where id = 2
+	//db.Delete(&Admin{})    // all delete
+
+	// 最後に全てをfetchし確認
+	db.Order("id DESC").Find(&list)
+	for _, v := range list {
+		fmt.Printf("%#v\n", v)
+	}
+}
+
+// テーブル名:adminsに対して、Admin or Adminsどちらの構造体名でもOK
+type Admin struct {
+	ID    int
+	Name  string
+	Age   int
+	Isman bool
 }
